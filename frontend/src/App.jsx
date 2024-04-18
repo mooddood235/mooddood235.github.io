19fdfc7a6da7379fb5b6eef3ab3aae02f5099830
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-04-17 23:40:11 -0400

    Moved 3d to react componenent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import {motion} from "framer-motion"
+import { useRef } from 'react';
+import * as THREE from 'three';
+import { MathUtils } from 'three';
 
-function App(){
+function App({resources}){
   const [state, setState] = useState({page:'home'});
   const [cursorPos, setCursorPos] = useState({x:0, y:0});
   const handleMouseMove = (e)=>{
@@ -11,7 +13,7 @@ function App(){
   return (
     <div id='app' onMouseMove={handleMouseMove}>
       <Nav/>
-      {state.page === 'home' ? <Home cursorPos={cursorPos}/> : null}
+      {state.page === 'home' ? <Home cursorPos={cursorPos} resources={resources}/> : null}
     </div>
   )
 }
@@ -24,7 +26,7 @@ function Nav(){
     </div>
   )
 }
-function Home({cursorPos}){
+function Home({cursorPos, resources}){
   const topOffset = (cursorPos.y / window.innerHeight) * 2.0 - 1.0;
   const leftOffset = (cursorPos.x / window.innerWidth) * 2.0 - 1.0;
   const strength = 2.0;
@@ -33,7 +35,6 @@ function Home({cursorPos}){
     top:`${10 + topOffset * strength}%`,
     left:`${-10 + leftOffset * strength}%`
   }
-  console.log(topOffset, leftOffset);
   return (
     <div id='home'>
       <div id='name'>
@@ -44,7 +45,75 @@ function Home({cursorPos}){
           Zeidan
         </div>
       </div>
+      <Three resources={resources}/>
     </div>
+  )
+}
+function Three({resources}){
+  const canvasRef = useRef(null);
+  
+  useEffect(()=>{
+    const renderWidth = window.innerWidth;
+    const renderHeight = window.innerHeight;
+  
+    const renderer = new THREE.WebGLRenderer({alpha:true}); 
+    renderer.setSize(renderWidth, renderHeight);
+    canvasRef.current.appendChild(renderer.domElement);
+    
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, renderWidth / renderHeight, 0.1, 1000 );
+    camera.position.z = 5;
+    
+    const geometry = CreateGeometry();
+    var material = CreateMaterial(resources);
+    
+    const object = new THREE.Mesh( geometry, material );
+    object.position.z = -30;
+    object.rotation.y = MathUtils.degToRad(-15);
+    object.rotation.z = MathUtils.degToRad(-15);
+    object.rotation.x = MathUtils.degToRad(-15);
+    scene.add( object );
+  
+    render();
+  
+    function CreateGeometry(){
+      const geometry = new THREE.TorusKnotGeometry(10, 3, 1500, 300, 2,3);
+      geometry.computeTangents();
+      return geometry;
+    }
+    function CreateMaterial(resources){
+      var material = new THREE.MeshPhysicalMaterial({
+        specularIntensity:1.0,
+        roughness:0.0,
+        color:new THREE.Color(0.1, 0.1, 0.1),
+        envMap:resources.envTexture
+      });
+      material.onBeforeCompile = function(shader){
+        shader.vertexShader = resources.vertexStr;
+        shader.uniforms.time = {value:0.0};
+        material.userData.shader = shader;
+      }
+      return material;
+    }
+    function render(){
+      const time = performance.now() / 1000.0;
+      requestAnimationFrame(render);
+  
+      if (material.userData.shader)
+        material.userData.shader.uniforms.time.value = time;
+  
+      object.rotation.z += MathUtils.degToRad(Math.sin(time) * 0.2);
+  
+      renderer.render(scene, camera);
+    }
+    // window.addEventListener('resize', function(){
+    //   camera.aspect = renderWidth / renderHeight;
+    //   camera.updateProjectionMatrix();
+    //   renderer.setSize(renderWidth, renderHeight);
+    // });
+  }, []);
+  return (
+    <div id='three' ref={canvasRef}/>
   )
 }
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -10,76 +10,13 @@ import { MathUtils } from 'three/src/math/MathUtils.js';
 const renderWidth = window.innerWidth;
 const renderHeight = window.innerHeight;
 
-GetResources(Main);
+//GetResources(Main);
 
-function Main(resources){
-  const renderer = new THREE.WebGLRenderer({alpha:true}); 
-  renderer.setSize(renderWidth, renderHeight);
-  
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(60, renderWidth / renderHeight, 0.1, 1000 );
-  camera.position.z = 5;
-  
-  const geometry = CreateGeometry();
-  var material = CreateMaterial(resources);
-  
-  const object = new THREE.Mesh( geometry, material );
-  object.position.z = -30;
-  object.rotation.y = MathUtils.degToRad(-15);
-  object.rotation.z = MathUtils.degToRad(-15);
-  object.rotation.x = MathUtils.degToRad(-15);
-  scene.add( object );
-
-  function render(){
-    const time = performance.now() / 1000.0;
-    requestAnimationFrame(render);
-
-    if (material.userData.shader)
-      material.userData.shader.uniforms.time.value = time;
-
-    object.rotation.z += MathUtils.degToRad(Math.sin(time) * 0.2);
-
-    renderer.render(scene, camera);
-  }
-  window.addEventListener('resize', function(){
-    camera.aspect = renderWidth / renderHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(renderWidth, renderHeight);
-  });
+GetResources((resources)=>{
   ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App resources={resources}/>
   )
-  //document.getElementById('three').appendChild(renderer.domElement);
-  render();  
-}
-
-function CreateGeometry(){
-  const geometry = new THREE.TorusKnotGeometry(10, 3, 1500, 300, 2,3);
-  //const geometry = new THREE.IcosahedronGeometry(10, 200);
-  // var indices = [];
-  // for (var i = 0; i < geometry.getAttribute('position').count; i++){
-  //   indices.push(i);
-  // }
-  // geometry.setIndex(indices);
-  geometry.computeTangents();
-  return geometry;
-}
-function CreateMaterial(resources){
-  var material = new THREE.MeshPhysicalMaterial({
-    specularIntensity:1.0,
-    roughness:0.0,
-    color:new THREE.Color(0.1, 0.1, 0.1),
-    envMap:resources.envTexture
-  });
-  material.onBeforeCompile = function(shader){
-    shader.vertexShader = resources.vertexStr;
-    shader.uniforms.time = {value:0.0};
-    material.userData.shader = shader;
-  }
-  return material;
-}
+})
 function GetResources(cb){
   api_getshaders(function(data0){
     const loader = new RGBELoader();
