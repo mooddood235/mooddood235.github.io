0bb7262443e78241e0ca5b42ce78a619dc3c721c
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-04-17 21:09:44 -0400

    Shadow moves with cursor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,31 @@ import './App.css'
 import TypeWriter from 'typewriter-effect'
 
 function App(){
+  const [cursorPos, setCursorPos] = useState({x:0, y:0});
+  const handleMouseMove = (e)=>{
+    setCursorPos({x:e.clientX, y:e.clientY});
+  }
   return (
-    <div id='app'>
-      <Home/>
+    <div id='app' onMouseMove={handleMouseMove}>
+      <Home cursorPos={cursorPos}/>
     </div>
   )
 }
-function Home(){
+function Home({cursorPos}){
+  const topOffset = (cursorPos.y / window.innerHeight) * 2.0 - 1.0;
+  const leftOffset = (cursorPos.x / window.innerWidth) * 2.0 - 1.0;
+  const strength = 2.0;
+
+  const shadowStyle = {
+    top:`${10 + topOffset * strength}%`,
+    left:`${-10 + leftOffset * strength}%`
+  }
   return (
     <div id='home'>
       <div id='name'>
         Mahmoud<br/>
         Zeidan
-        <div id='shadow'>
+        <div id='shadow' style={shadowStyle}>
           Mahmoud<br/>
           Zeidan
         </div>
