c583855632d1bc355c35acf95b26d50cf747a8be
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-05-08 20:33:48 -0400

    Changed readme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -181,14 +181,12 @@ function About(){
       <div id='about_content'>
         <div id='about_title'>About Me</div>
         <div id='about_textblock'>
-        Greetings! I'm <JetBrainsBold text='Mahmoud Zeidan'/>, currently pursuing my fifth year in Computer Science at
-        the University of Toronto. My love for logic, mathematics, and art finds its home in the world
-        of Computer Science, where I merge and manifest my passions into complex and innovative projects.
-        Whether I'm diving into the the world of <JetBrainsBold text='graphics rendering'/>
-        , <JetBrainsBold text='game development'/>, <JetBrainsBold text='web development'/>,
-        or <JetBrainsBold text='programming languages'/>, I'm always on the lookout for new challenges and avenues of exploration.
-        <br/><br/>
-        Curious about my endeavors? Feel free to explore my projects page and GitHub. 
+          My name is <JetBrainsBold text='Mahmoud Zeidan'/>. I am a 5<sup>th</sup> year 
+          Computer Science student at the University of Toronto.
+          I like to explore how technology works on a fundemental level. I enjoy building systems
+          and tools that enable others to create software. My focus areas include 
+          <JetBrainsBold text=' computer graphics'/>, <JetBrainsBold text='game development'/>,
+          <JetBrainsBold text=' web development'/>, and <JetBrainsBold text='programming languages'/>.
         </div>
       </div>
     </div>
