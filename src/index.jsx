a1706e995d6cf44c5cefcfe5d3736132f9e1a086
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-05-09 19:56:36 -0400

    atempty to fix hdr load problem

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ GetResources((resources)=>{
 })
 function GetResources(cb){
   const loader = new RGBELoader();
-  loader.load('src/assets/studio.hdr', (envTexture) =>{
+  loader.load('./src/assets/studio.hdr', (envTexture) =>{
     envTexture.mapping = THREE.EquirectangularReflectionMapping;
     fetch('src/vertex.glsl')
     .then(response=>response.text())
