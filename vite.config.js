690511e693da3d8c6d3752b0d44a12d95250b023
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-05-09 20:10:56 -0400

    removed base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"https://mooddood235.github.io/personal-website/"
+  plugins: [react()]
 })
