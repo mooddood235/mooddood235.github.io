9a10ecd8a7a314501ce1fe39b599793d77019329
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2024-05-09 20:06:54 -0400

    Changed base to full path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/personal-website"
+  base:"https://mooddood235.github.io/personal-website/"
 })
